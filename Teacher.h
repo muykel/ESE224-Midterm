330ac95ee0900636b211edeb3fc0380b53d56af2
MatMecz02 <118077149+MatMecz02@users.noreply.github.com>
2022-11-12 17:37:07 -0500

    Update Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -5,23 +5,23 @@ using namespace std;
 class Teacher
 {
 private:
-string username, password;
-static int maxCopies, maxBorrowPeriod;
-vector<Book> copies;
+	string username, password;
+	static int maxCopies, maxBorrowPeriod;
+	vector<Book> copies;
 public:
-Teacher();
-Teacher(string u, string p);
-string getusername();
-string getpassword();
-int getmaxcopies();
-int getmaxborrowperiod();
-vector<Book> getcopies();
-void setusername(string a);
-void setpassword(string a);
-void borrowbooks(int id);
-void returnbooks(int id);
-void requestcopy(long int isbn, string title, string author, string category);
-void deletecopy(int id);
-void operator <<(string file_name, Teacher& t1);
-Teacher operator >>(string file_name, Teacher& t1);
+	Teacher();
+	Teacher(string u, string p);
+	string getusername();
+	string getpassword();
+	int getmaxcopies();
+	int getmaxborrowperiod();
+	vector<Book> getcopies();
+	void setusername(string a);
+	void setpassword(string a);
+	void borrowbooks(int id);
+	void returnbooks(int id);
+	void requestcopy(long int isbn, string title, string author, string category);
+	void deletecopy(int id);
+	void operator <<(Teacher& t1);
+	Teacher operator >>( Teacher& t1);
 };
