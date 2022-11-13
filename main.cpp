99b6284445df0c018ed3c138ebbb4472f4e7ca0b
MatMecz02 <118077149+MatMecz02@users.noreply.github.com>
2022-11-13 18:28:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,24 +5,25 @@
 using namespace std;
 
 int main() {
-        fstream search;
-        cout << "\n\n\t\t\t *****Libraray Management System Login Page*****\n\n\n" << endl;
-        cout << "Current time is:  " <<
-                auto givetime = chrono::system_clock::to_time_t(chrono::system_clock::now());
-        cout << ctime(&givetime) << endl;
+    fstream search;
+    cout << "\n\n\t\t\t *****Libraray Management System Login Page*****\n\n\n" << endl;
+    cout << "Current time is:  ";
+    auto givetime = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    cout << ctime(&givetime) << endl;
+    cout << givetime;
 
-        ifstream bookin;
-        bookin.open("book.txt");
-        if(bookin.fail())
-        {
-                cerr<<"Can not open book file";
-                exit(1);
-        }
-        ifstream studentin;
-        studentin.open("student.txt");
-        if (studentin.fail())
-        {
-                cerr << "Can not open student file";
-                exit(1);
-        }
+    ifstream bookin;
+    bookin.open("book.txt");
+    if (bookin.fail())
+    {
+        cerr << "Can not open book file";
+        exit(1);
+    }
+    ifstream studentin;
+    studentin.open("student.txt");
+    if (studentin.fail())
+    {
+        cerr << "Can not open student file";
+        exit(1);
+    }
 }
